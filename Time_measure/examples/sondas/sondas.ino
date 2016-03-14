0f63742ea4cb108230c6f9fa35df65f7c1fd843f
André Torres <andregtorres@gmail.com>
2016-03-14 12:44:36 +0000

    Fully offline probe reading code
    created from sondas_internet, removed thingspeak lines

--- Time_measure/examples/sondas/sondas.ino
+++ Time_measure/examples/sondas/sondas.ino
@@ -1,8 +1,4 @@
 #include <Time_measure.h>
-#include <SPI.h>
-#include <Ethernet2.h>
-
-#include "thingspeak.h"
 
 #define pin_IC 4
 void setup() {
@@ -10,15 +6,11 @@ void setup() {
   configureIC(0);
   Serial.begin(9600);                // initialize serial communication at 9600 bits per second
   while (!Serial);
-    startEthernet(); // Start Ethernet on Arduino
 }
 
 void loop() {
-    thingspeak_loop();
   float a=measureTime();
   Serial.print(1/a,5);
   Serial.println(" Hz");
-   String analogValue0 = String(1/a, DEC);
-   updateThingSpeak("field2="+analogValue0+"&field2="+analogValue0);
   delay(4000);
 }
